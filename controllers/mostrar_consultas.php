f176adab132565660a46f9e34f16ef848273bbe0
Jesús Alexis Ramírez Navor <jesusrn221099@gmail.com>
2021-10-26 15:55:41 -0500

    Backend Historial

--- controllers/mostrar_consultas.php
+++ controllers/mostrar_consultas.php
@@ -24,8 +24,8 @@ if ($resultado->num_rows > 0) {
                 <tr>
                     <th>" . $fila['fechaLlegada'] . "</th>
                     <th><a href='../historial/index.php?id=".$fila['id']."'>" . $fila['enfermedad'] . "</a></th>
-                </tr>           //El link de aquí es el que debes cambiar para la redirección
-            ";
+                </tr>          
+            ";              //El link de aquí es el que debes cambiar para la redirección
     }
 
     $salida .= "
